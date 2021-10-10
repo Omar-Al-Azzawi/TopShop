efa81e61f2faf596cd806bc5786853d94f627408
Omar Al-Azzawi <alazzawiomar01@gmail.com>
2021-10-10 21:18:12 +0300

    add buttom add to cart to item component

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,11 +1,14 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import "./Item.css";
 import { useParams } from "react-router-dom";
 import Rating from "./Rating";
+import CartContext from "../Context/Cart/CartContext";
 
 function Item() {
   const { id } = useParams();
   const [items, setItems] = useState("");
+  const { addToCart } = useContext(CartContext);
+
   useEffect(() => {
     fetch("https://fakestoreapi.com/products")
       .then((res) => res.json())
@@ -29,6 +32,12 @@ function Item() {
                   <p>€{item.price}</p>
                   <Rating className="rate" value={item.rating.rate} />
                   <p>{item.rating.count} pes</p>
+                  <button
+                    className="ProductCard__button"
+                    onClick={() => addToCart(item)}
+                  >
+                    Add to cart <i className="fa fa-shopping-cart"></i>
+                  </button>
                 </div>
               </div>
             </div>
