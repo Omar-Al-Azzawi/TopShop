b4104eb319301a6f7276f300fcc0c124544ac80b
Omar Al-Azzawi <alazzawiomar01@gmail.com>
2021-10-23 20:29:30 +0300

    working on the search bar

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -5,16 +5,38 @@ import "./HomeScreen.css";
 
 function HomeScreen() {
   const [items, setItems] = useState("");
+  const [search, setSearch] = useState("");
+
   useEffect(() => {
     fetch("https://fakestoreapi.com/products")
       .then((res) => res.json())
       .then((data) => setItems(data));
   }, []);
+
   return (
     <>
+      <div className="search">
+        <input
+          type="text"
+          placeholder="Serach..."
+          onChange={(e) => {
+            setSearch(e.target.value);
+          }}
+        />
+      </div>
       <div className="products__wrapper">
         {items &&
-          items.map((item) => <ProductsCart key={item.id} item={item} />)}
+          items
+            .filter((val) => {
+              if (search === "") {
+                return val;
+              } else if (
+                val.title.toLowerCase().includes(search.toLowerCase())
+              ) {
+                return val;
+              }
+            })
+            .map((item) => <ProductsCart key={item.id} item={item} />)}
       </div>
       <button
         className="ProductCard__button"
